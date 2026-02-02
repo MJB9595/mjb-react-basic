76dfd1d9cc3c8499376f9b29a81239ce77bebd01
MJB9595 <wansja7870@gmail.com>
2026-02-02 11:53:32 +0900

    컴포넌트 폴더가 src 바깥에 있음으로 import를 하나이상 못하는 문제를 해결

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+import M_side from './components/M_side'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Header/>
+      <M_side/>
+      <h2>app is here</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/M_side.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const M_side = () => {
+  return (
+    <div>
+        <h3>asdie is here</h3>
+    </div>
+  )
+}
+
+export default M_side
